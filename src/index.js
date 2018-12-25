e2b688f51ba91644e21d74f4951f595aeda10041
Andrei Voicu <dreivoicu@gmail.com>
2018-12-25 14:59:03 +0200

    details for one pokemon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 // 1. add `react-cache` as a project dependency
 // 2. import resource creators from react-cache
 import { unstable_createResource as createResource } from 'react-cache'
 
-// 3. create a pokemon resource that fetches data
-let PokemonCollectionResource = createResource(() =>
-  fetch('https://pokeapi.co/api/v2/pokemon/').then(res => res.json())
+let PokemonResource = createResource(() =>
+  fetch('https://pokeapi.co/api/v2/pokemon/1/').then(res => res.json())
 )
 
 function PokemonListItem({ className, component: Component = 'li', ...props }) {
@@ -19,15 +18,49 @@ function PokemonListItem({ className, component: Component = 'li', ...props }) {
 }
 
 // 4. pull your resource-reading UI into a component
-function PokemonList() {
+function PokemonDetail() {
+  const data = PokemonResource.read()
+  const { name, abilities, height, moves, sprites, stats, types, weight } = data
   return (
     <section>
-      <h3>Pokemons:</h3>
+      <h3>Pokemon Details:</h3>
+      <img src={sprites.front_default} alt="bulbasaur" />
       <ul>
-        {/* 5. read resource data */}
-        {PokemonCollectionResource.read().results.map(pokemon => (
-          <PokemonListItem key={pokemon.name}>{pokemon.name}</PokemonListItem>
-        ))}
+        <li>name: {name}</li>
+        <li>
+          abilities:
+          <ul>
+            {abilities.map(({ ability: { name } }) => (
+              <PokemonListItem key={name}>{name}</PokemonListItem>
+            ))}
+          </ul>
+        </li>
+        <li>height: {height}</li>
+        <li>
+          moves:
+          <ul>
+            {moves.map(({ move: { name } }) => (
+              <PokemonListItem key={name}>{name}</PokemonListItem>
+            ))}
+          </ul>
+        </li>
+        <li>
+          stats:
+          <ul>
+            {stats.map(({ stat: { name } }) => (
+              <PokemonListItem key={name}>{name}</PokemonListItem>
+            ))}
+          </ul>
+        </li>
+        <li>
+          types:
+          <ul>
+            {types.map(({ type: { name } }) => (
+              <PokemonListItem key={name}>{name}</PokemonListItem>
+            ))}
+          </ul>
+        </li>
+        <li>weight: {weight}</li>
       </ul>
     </section>
   )
@@ -44,9 +77,9 @@ function App() {
       </h1>
       {/* 6. wrap your resource-reading component in the Suspense component */}
       {/*    ...and provide a fallback */}
-      <React.Suspense fallback={<div>...loading</div>}>
-        <PokemonList />
-      </React.Suspense>
+      <Suspense fallback={<div>...loading</div>}>
+        <PokemonDetail />
+      </Suspense>
     </div>
   )
 }
